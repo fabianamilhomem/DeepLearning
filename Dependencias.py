e21359d8825d55d68d5d1a1a71ab7dc11173f4de
fabianamilhomem <fabianasilvamilhomem@gmail.com>
2025-12-14 18:36:17 -0300

    Projeto Final de Deep Learning - Fabiana e Mairton

--- Dependencias.py
+++
@@ -1,9 +0,0 @@
-pip install requests beautifulsoup4 lxml PyPDF2 pandas
-# (Opcional) para modo Selenium quando houver páginas altamente dinâmicas:
-pip install selenium webdriver-manager
-
-python -m spacy download pt_core_news_md
-
-# Dicas
-# 1) criar o venv
-python -m venv .venv
